d5337d14551eedf1d0b55693ac55f5eaab317c54
Alana Alexander <laninsky@gmail.com>
2018-05-13 20:28:19 +1200

    Update GBS_SNP_filter.R

--- GBS_SNP_filter.R
+++ GBS_SNP_filter.R
@@ -2,10 +2,12 @@ library(tidyverse)
 parameters <- read.table("GBS_SNP_filter.txt")
 basename <- gsub(".vcf","",parameters[1,1])
 filelist <- list.files()
+logfilename <- paste(basename,".log",sep="")
 
 if (!((paste(basename,".biallelic.vcf",sep="")) %in% filelist)) {#1A  what to do if biallelic doesn't exist
   temp <- read_tsv("temp",col_names=TRUE)
-  check <- temp %>% mutate(commacount=str_count(ALT,",")) %>% filter(., commacount==0) %>% 
+  
+  temp <- temp %>% mutate(commacount=str_count(ALT,",")) %>% filter(., commacount==0) %>% select(.,-commacount)
   
   
   
