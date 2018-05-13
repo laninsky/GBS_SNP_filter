a08a1160a26cc3a4f7841be802ecc2d4d6b6a6cf
Alana Alexander <laninsky@gmail.com>
2018-05-13 20:59:06 +1200

    Update GBS_SNP_filter.R

--- GBS_SNP_filter.R
+++ GBS_SNP_filter.R
@@ -15,7 +15,9 @@ if (!((paste(basename,".biallelic.vcf",sep="")) %in% filelist)) {#1A  what to do
   write((dim(temp)[1]-1),logfilename,append=TRUE)  
   write_delim(temp,(paste(basename,".biallelic.vcf",sep="")),delim="\t",append=TRUE,col_names=TRUE)  
 } else { #1AB
-  headerrows <- read_tsv("header_row.txt",col_names=FALSE)
-  numberofheaders <- dim(headerrows)[1]
-  temp <- read_tsv((paste(basename,".biallelic.vcf",sep="")),col_names=TRUE,skip=numberofheaders)
+  if (!((paste(basename,".oneSNP.vcf",sep="")) %in% filelist)) {#2A reading in *biallelic.vcf if *oneSNP.vcf doesn't exist
+    headerrows <- read_tsv("header_row.txt",col_names=FALSE)
+    numberofheaders <- dim(headerrows)[1]
+    temp <- read_tsv((paste(basename,".biallelic.vcf",sep="")),col_names=TRUE,skip=numberofheaders)
+  } #2B  
 } #1B  
