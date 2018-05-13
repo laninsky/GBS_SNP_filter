01241dd60ce0c5a17f163e6a13da6871fdeff8c5
Alana Alexander <laninsky@gmail.com>
2018-05-13 19:48:23 +1200

    Update GBS_SNP_filter.R

--- GBS_SNP_filter.R
+++ GBS_SNP_filter.R
@@ -1,1 +1,10 @@
+library(tidyverse)
 parameters <- read.table("GBS_SNP_filter.txt")
+basename <- gsub(".vcf","",parameters[1,1])
+filelist <- list.files()
+
+if (!((paste(basename,".biallelic.vcf",sep="")) %in% filelist)) {#1A  what to do if biallelic doesn't exist
+  temp <- read_table("temp",col_names=TRUE)
+  temp <- separate(temp,col=1,sep="\t",remove=TRUE)
+  temp <- as_tibble(read.table("temp",header=TRUE,stringsAsFactors=FALSE))
+} #1B  
