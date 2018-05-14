0f5d0b729d23551505dd4903ce9e124b9554c9d4
Alana Alexander <laninsky@gmail.com>
2018-05-14 21:09:38 +1200

    Update GBS_SNP_filter.R

--- GBS_SNP_filter.R
+++ GBS_SNP_filter.R
@@ -12,7 +12,9 @@ if (!((paste(basename,".biallelic.vcf",sep="")) %in% filelist)) {#1A  what to do
   temp <- temp %>% mutate(commacount=str_count(ALT,",")) %>% filter(., commacount==0) %>% select(.,-commacount)
   write(format(Sys.time(),usetz = TRUE),logfilename,append=TRUE)
   write(paste(basename,".biallelic.vcf has the following number of SNPs:",sep=""),logfilename,append=TRUE)
-  write((dim(temp)[1]-1),logfilename,append=TRUE)  
+  write((dim(temp)[1]-1),logfilename,append=TRUE) 
+  headerrows <- read_tsv("header_row.txt",col_names=FALSE)
+  write_delim(headerrows,(paste(basename,".biallelic.vcf",sep="")),delim="\t",append=TRUE,col_names=TRUE)  
   write_delim(temp,(paste(basename,".biallelic.vcf",sep="")),delim="\t",append=TRUE,col_names=TRUE)  
 } else { #1AB
   if (!((paste(basename,".oneSNP.vcf",sep="")) %in% filelist)) {#2A reading in *biallelic.vcf if *oneSNP.vcf doesn't exist
