5c876dc24b74608d459583a24460c003e16fe85a
Alana Alexander <laninsky@gmail.com>
2018-05-14 21:35:28 +1200

    Update GBS_SNP_filter.sh

--- GBS_SNP_filter.sh
+++ GBS_SNP_filter.sh
@@ -7,6 +7,7 @@ then
 else 
   grep "##" $filename >> header_row.txt;
   headerlineno=`wc -l header_row.txt | awk '{print $1}'`;
+  headerlineno=$((headerlineno+1))
   tail -n +$headerlineno $filename > temp;
 fi	
 
