56e16708c052ea1d315597fafd0a7655c531bca0
Alana Alexander <laninsky@gmail.com>
2018-05-14 21:07:57 +1200

    Update GBS_SNP_filter.sh

--- GBS_SNP_filter.sh
+++ GBS_SNP_filter.sh
@@ -4,6 +4,8 @@ basename=`echo $filename | sed 's/.vcf//g'`
 if [ -f $basename.biallelic.vcf ]; then
 else
   grep "##" $filename >> header_row.txt	
+  headerlineno=`wc -l header_row.txt | awk '{print $1}'`
+  tail -n +$headerlineno $filename > temp
 fi	
 
 Rscript GBS_SNP_filter.R
