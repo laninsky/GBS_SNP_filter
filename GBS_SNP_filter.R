5ec700427643a71af93b7c9bac12e5cfc5a8acc0
Alana Alexander <laninsky@gmail.com>
2018-05-15 11:07:51 +1200

    Update GBS_SNP_filter.R

--- GBS_SNP_filter.R
+++ GBS_SNP_filter.R
@@ -34,14 +34,36 @@ if (!((paste(basename,".oneSNP.vcf",sep="")) %in% filelist)) {#3A: if oneSNP.vcf
   notduplicated <- notduplicated %>%  mutate_at(vars((dim(temp)[2]+1):(dim(notduplicated)[2])),funs(as.numeric)) 
   duplicated <- temp %>% filter(., (`#CHROM` %in% duplicatedloci))
   duplicated <- duplicated %>% mutate_at(vars(10:dim(temp)[2]), .funs = funs(cov = gsub(":.*","",gsub("^.*?:","", . )))) 
-  duplicated <- duplicated %>%  mutate_at(vars((dim(temp)[2]+1):(dim(duplicated)[2])),funs(as.numeric)) 
+  duplicated <- duplicated %>%  mutate_at(vars((dim(temp)[2]+1):(dim(duplicated)[2])),funs(as.numeric))
+  duplicated_reduced <- NULL
   for (i in duplicatedloci) {
     temptemp <- duplicated %>% filter(., (`#CHROM` %in% i))
     zerocounts <- unlist(lapply(1:(dim(temptemp)[1]),function(x){sum(temptemp[x,(dim(temp)[2]+1):(dim(temptemp)[2])]==0)}))
     temptemp <- temptemp[(which(zerocounts==min(zerocounts))),]    
     covcounts <- rowSums(temptemp[,(dim(temp)[2]+1):(dim(temptemp)[2])])
     temptemp <- temptemp[(which(covcounts==min(covcounts))),]
-  
+    if (dim(temptemp)[1]>1) { #4A: if we haven't narrowed down to one SNP for this locus
+      randomSNP <- sample(1:dim(temptemp)[1], 1, replace=FALSE)
+      duplicated_reduced <- bind_rows(duplicated_reduced, temptemp[randomSNP,])
+    } else { #4AB: if we HAVE narrowed to one SNP for this locus
+      duplicated_reduced <- bind_rows(duplicated_reduced, temptemp)
+    } #4B 
+  }
+  origcolnumber <- dim(temp)[2]
+  temp <- bind_rows(notduplicated,duplicated_reduced)
+  write(format(Sys.time(),usetz = TRUE),logfilename,append=TRUE)
+  write(paste(basename,".oneSNP.vcf has the following number of SNPs:",sep=""),logfilename,append=TRUE)
+  write((dim(temp)[1]),logfilename,append=TRUE) 
+  write.table(headerrows,(paste(basename,".oneSNP.vcf",sep="")),quote=FALSE,row.names=FALSE,col.names=FALSE)  
+  write_delim(temp[,1:origcolnumber],(paste(basename,".oneSNP.vcf",sep="")),delim="\t",append=TRUE,col_names=TRUE)  
+} else { #3AB if oneSNP.vcf does exist
+    headerrows <- read_tsv("header_row.txt",col_names=FALSE)
+    numberofheaders <- dim(headerrows)[1]
+    temp <- read_tsv((paste(basename,".oneSNP.vcf",sep="")),col_names=TRUE,skip=numberofheaders)
+    origcolnumber <- dim(temp)[2]
+    temp <- temp %>% mutate_at(vars(10:dim(temp)[2]), .funs = funs(cov = gsub(":.*","",gsub("^.*?:","", . )))) 
+    temp <- temp %>%  mutate_at(vars((origcolnumber+1):(dim(temp)[2])),funs(as.numeric))
+} #3B
   
   # Want to create a vector listing the loci with more than one SNP (duplicated)
   # write a new matrix which has the loci that DON'T fall in this group
