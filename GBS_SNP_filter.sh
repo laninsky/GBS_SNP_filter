d1f3bdd53ab2799d6a408c81c7fdc854d2f1782c
Alana Alexander <laninsky@gmail.com>
2018-05-18 10:10:53 +1200

    Update GBS_SNP_filter.sh

--- GBS_SNP_filter.sh
+++ GBS_SNP_filter.sh
@@ -11,4 +11,21 @@ else
   tail -n +$headerlineno $filename > temp;
 fi	
 
-Rscript GBS_SNP_filter.R
+Rscript GBS_SNP_filter_HWE.R
+
+N=`tail -n 1 GBS_SNP_filter.txt`
+for i in `ls *pop.vcf`; do
+    (
+        # .. do your stuff here
+        echo "starting task $i.."
+        sleep $(( (RANDOM % 3) + 1))
+    ) &
+
+    # allow only to execute $N jobs in parallel
+    if [[ $(jobs -r -p | wc -l) -gt $N ]]; then
+        # wait only for first job
+        wait -n
+    fi
+
+done
+wait
