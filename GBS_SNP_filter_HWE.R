4b34dc368111e765d438750687964a561df0260b
Alana Alexander <laninsky@gmail.com>
2018-05-18 10:27:05 +1200

    Update GBS_SNP_filter_HWE.R

--- GBS_SNP_filter_HWE.R
+++ GBS_SNP_filter_HWE.R
@@ -163,7 +163,19 @@ if (!((paste(basename,".",parameters[2,1],"_",parameters[3,1],".HWE.vcf",sep="")
 }
 
 
-robin.0.85_0.9_0.5
+popmap <- read.table("popmap.txt",header=FALSE,stringsAsFactors=FALSE)
+popnames <- unique(popmap[,2])
+for (k in 1:length(popnames)) {
+   tempK <- select(temp, c((1:10),which(names(temp) %in% (popmap[(which(popmap[,2]==popnames[k])),1]))))
+   origcolnumber <- dim(tempK)[2]
+   tempK <- mutate_at(tempK,vars(10:origcolnumber),.funs = funs(genotype = gsub(":.*","", . )))     
+   tempK <- mutate(tempK, hom1 = rowSums(tempK[,(origcolnumber+1):(dim(tempK)[2])] == "1/1"))
+   tempK <- mutate(tempK, het = ((rowSums(tempK[,(origcolnumber+1):(dim(tempK)[2]-1)] == "0/1")+(rowSums(tempK[,(origcolnumber+1):(dim(tempK)[2]-1)] == "1/0")))))
+   tempK <- mutate(tempK, hom0 = rowSums(tempK[,(origcolnumber+1):(dim(tempK)[2])-2] == "0/0"))
+   tempK <- filter(tempK,(!(((hom1+het)==0))|((hom0+het)==0)))
+   write.table(headerrows,(paste(basename,".",parameters[2,1],"_",parameters[3,1],".",popnames[k],".pop.vcf",sep="")),quote=FALSE,row.names=FALSE,col.names=FALSE)  
+   write_delim(tempK[,1:origcolnumber],(paste(basename,".",parameters[2,1],"_",parameters[3,1],".",popnames[k],".pop.vcf",sep="")),delim="\t",append=TRUE,col_names=TRUE)    
+}
 
 
 
