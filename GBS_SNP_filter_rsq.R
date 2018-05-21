34b22359c4328865e665b5b485871bdc0a6685d6
Alana Alexander <laninsky@gmail.com>
2018-05-21 22:02:57 +1200

    Update GBS_SNP_filter_rsq.R

--- GBS_SNP_filter_rsq.R
+++ GBS_SNP_filter_rsq.R
@@ -34,16 +34,27 @@ names(SNP_record) <- c("Locus_1","Locus_2",popnames)
 
 SNP_record <- filter(SNP_record, (dim(SNP_record)[2]-2)-rowSums(is.na(SNP_record[,3:(dim(SNP_record)[2])]))>=as.numeric(parameters[6,1]))
 
+SNP_record <- cbind(SNP_record, (matrix(NA,ncol=1,nrow=dim(SNP_record)[1])))
+names(SNP_record)[(dim(SNP_record)[2])] <- "removed"
+
 j <- 1
 SNP_length <- dim(SNP_record)[1]
+
 while (j <= SNP_length) {
   zero_one_count <- sum(temp[(which(temp$`#CHROM` %in% SNP_record[j,1])),((origcolnumber+1):(dim(temp)[2]))]==0)
   zero_two_count <- sum(temp[(which(temp$`#CHROM` %in% SNP_record[j,2])),((origcolnumber+1):(dim(temp)[2]))]==0)
   if (zero_one_count > zero_two_count) {
+    temp <- temp[-(which(temp$`#CHROM` %in% SNP_record[j,1])),]
+    SNP_record[j,(dim(SNP_record)[2])] <- SNP_record[j,1]    
+    todelete <- c((which(SNP_record[,1] %in% SNP_record[j,1])),(which(SNP_record[,2] %in% SNP_record[j,1])))
+    
+    
+    SNP_record[j,2]
     # record SNP_length, and that [j,1] is being removed. Remove [j,1] from temp, and then delete any further temp[j,1]s from SNP_record
   } else {
     if (zero_two_count > zero_one_count) {
       # same but [j,2]
+      j <- j + 1
     } else {
       if (zero_two_count==zero_one_count) {
         #if they do equal each other, then we need to do overall coverage
