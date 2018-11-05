4a2e2a81e174c4e51cf3533c2e615aa9dadcc527
Alana Alexander <laninsky@gmail.com>
2018-11-06 10:20:54 +1300

    Update sMLH_MLH.R

--- utilities/sMLH_MLH.R
+++ utilities/sMLH_MLH.R
@@ -4,14 +4,17 @@ sMLH_MLH <- function(vcf_name) {
   if (!require('vcfR')) install.packages('vcfR'); library('vcfR')
   if (!require('reshape')) install.packages('reshape'); library('reshape')
 
-# Following instructions for importing and reformatting data at https://cran.r-project.org/web/packages/inbreedR/vignettes/inbreedR_step_by_step.html
+  # Following instructions for importing and reformatting data at https://cran.r-project.org/web/packages/inbreedR/vignettes/inbreedR_step_by_step.html
   print("Reading in file")
   vcf <- read.vcfR(vcf_name, verbose = FALSE )
   print("Extracting genotypes")
   gt <- extract.gt(vcf)
   gt <- as.data.frame(t(gt), stringsAsFactors = FALSE)
   gt[gt == "."] <- NA
-  print("Converting formats")
+  print("Finding and removing any loci with no data for either allele. This step may take a while.")
+  all_NAs <- unlist(lapply(1:(dim(gt)[2]),function(x){all(is.na(gt[1:(dim(gt)[1]),x]))})) 
+  gt <- gt[,which(all_NAs[]==FALSE)]  
+  print("Converting formats. This step will also take a while.")  
   snp_geno <- do.call(cbind, apply(gt, 2, function(x) colsplit(x, "/", c("a","b"))))
   snp_genotypes <- inbreedR::convert_raw(snp_geno)
   print("If the following prints as TRUE, your genotypes are formatted correctly")                                   
