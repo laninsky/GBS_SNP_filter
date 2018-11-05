1a215adeaab267e2c7d7d858e74558138a6d8673
Alana Alexander <laninsky@gmail.com>
2018-11-06 12:37:21 +1300

    Update pop_He.R

--- utilities/pop_He.R
+++ utilities/pop_He.R
@@ -5,6 +5,10 @@ pop_He <- function(vcf_name,min_sample_size) {
   popsum <- gt.to.popsum(vcf)
   basename <- gsub(".vcf","",vcf_name)
   popsum <- popsum[(which(popsum[,1]>=min_sample_size)),]
+  print(paste("The average He across all loci in ",vcf_name," is: ",mean(popsum[,3]),sep=""))
+  print(paste("The effective number of alleles averaged across all loci in ",vcf_name," is: ",mean(popsum[,4]),sep=""))
+  print("The per locus values have been written out to:")
+  print(paste(basename,".",min_sample_size,".He.txt",sep=""))
   write.table(popsum,(paste(basename,".",min_sample_size,".He.txt",sep="")),quote=FALSE,row.names=FALSE)  
 
 }  
