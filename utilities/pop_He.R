579b0bb209f4498c59699a8a7a32fac1b026e242
Alana Alexander <laninsky@gmail.com>
2018-11-06 10:41:29 +1300

    Update pop_He.R

--- utilities/pop_He.R
+++ utilities/pop_He.R
@@ -4,7 +4,7 @@ pop_He <- function(vcf_name,min_sample_size) {
   vcf <- read.vcfR(vcf_name, verbose = FALSE )
   popsum <- gt.to.popsum(vcf)
   basename <- gsub(".vcf","",vcf_name)
-  popsum <- popsum[(which(popsum[,1]>=sample_size)),]
+  popsum <- popsum[(which(popsum[,1]>=min_sample_size)),]
   write.table(headerrows,(paste(basename,".",popnames[k],".",min_sample_size,".He.txt",sep="")),quote=FALSE,row.names=FALSE)  
 
 }  
