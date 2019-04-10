3e2ce79afe637bb3d7d78d00eee135d7555ed11b
Alana Alexander <laninsky@gmail.com>
2019-04-11 09:51:39 +1200

    Create GBS_SNP_filter_chrom_modifier.R

---
+++ GBS_SNP_filter_chrom_modifier.R
@@ -0,0 +1,14 @@
+# Loading in required packages
+if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
+if (!require('readr')) install.packages('readr'); library('readr')
+if (!require('stringr')) install.packages('stringr'); library('stringr')
+if (!require('tibble')) install.packages('tibble'); library('tibble')
+#library(tidyverse) # I've had issues loading library(tidyverse) and R crashing using the sbatch system
+
+mapfile <- read_tsv(list.files(pattern="*.map$"),col_names=FALSE)
+
+if (is.numeric(mapfile$X1)) {
+  mapfile$X1 <- paste("GBS_SNP_filter_",mapfile$X1,sep="")
+}
+
+write_tsv(mapfile,list.files(pattern="*.map$"),col_names=FALSE)
