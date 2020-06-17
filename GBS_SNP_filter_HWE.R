ee0e943a42ae454c379f206ad96bb99b8f78cca8
Alana Alexander <laninsky@gmail.com>
2020-06-17 15:51:42 +1200

    Update GBS_SNP_filter_HWE.R
    Updating version number after tweaking funs() calls to list() in R code

--- GBS_SNP_filter_HWE.R
+++ GBS_SNP_filter_HWE.R
@@ -1,4 +1,4 @@
-#GBS_SNP_filter v1.15
+#GBS_SNP_filter v1.16
 # Loading in required packages
 if (!require('dplyr')) install.packages('dplyr'); library('dplyr')
 if (!require('readr')) install.packages('readr'); library('readr')
